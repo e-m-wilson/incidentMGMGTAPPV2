26b462a27e9df8a0f163108e8150b55a995dddbc
Ethan Wilson <ethanwilson1991@gmail.com>
2025-11-05 13:44:44 -0600

    update

--- client/src/AuthProvider.jsx
+++ client/src/AuthProvider.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 export const AuthContext = createContext({
   isLogged: false,
   checking: true,
-  //reload: () => {},
-  //logout: () => {},
+  reload: () => {},
+  logout: () => {},
   login: () => {},
 });
 
@@ -17,28 +17,27 @@ export function AuthProvider({ children }) {
     window.location.href = "http://localhost:3001/auth/login";
   }, []);
 
-  //   const logout = useCallback(async () => {
-  //     await axios.get("http://localhost:3001/auth/logout", {
-  //       withCredentials: true,
-  //     });
-  //     setIsLogged(false);
-  //   }, []);
+  const logout = useCallback(async () => {
+    await axios.get("http://localhost:3001/auth/logout", {
+      withCredentials: true,
+    });
+    setIsLogged(false);
+  }, []);
 
-  // const reload = useCallback(async () => {
-  //     setChecking(true);
-  //     const r = await axios.get('http://localhost:3001/auth/status', {
-  //         withCredentials: true
-  //     });
-  //     setIsLogged(Boolean(r.data.authenticated));
+  const reload = useCallback(async () => {
+    setChecking(true);
+    const r = await axios.get("http://localhost:3001/auth/status", {
+      withCredentials: true,
+    });
+    setIsLogged(Boolean(r.data.authenticated));
+  }, []);
 
-  // }, [])
-
-  // useEffect(() => {
-  //     reload();
-  // }, [reload])
+  useEffect(() => {
+    reload();
+  }, [reload]);
 
   return (
-    <AuthContext.Provider value={{ isLogged, checking, login }}>
+    <AuthContext.Provider value={{ isLogged, checking, login, reload, logout }}>
       {children}
     </AuthContext.Provider>
   );
